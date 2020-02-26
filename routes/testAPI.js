76ba5c45d07cb6f43664023c5235be0185d60664
José Luis Vargas <joseluisvargas@Joses-MacBook-Pro.local>
2020-02-26 17:23:34 -0600

    conection with db2

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,13 +3,24 @@ var router = express.Router();
 var ibmdb = require('ibm_db');
 
 router.get('/', function(req, res, next) {
-    var connString = "DATABASE=INNOLABD;HOSTNAME=169.55.58.245;PORT=50001;PROTOCOL=TCPIP;UID=qfind;PWD=good4you;Security=SSL;SSLServerCertificate=/etc/ssl/certs/app/db2.ssl.file;";
+    //var connString = "DATABASE=INNOLABD;HOSTNAME=169.55.58.245;PORT=50001;PROTOCOL=TCPIP;UID=qfind;PWD=good4you;Security=SSL;SSLServerCertificate=/etc/ssl/certs/app/db2.ssl.file;";
+    connString = "DATABASE=SBYSA13;HOSTNAME=sbysa13.rochny.ibm.com;PORT=446;PROTOCOL=TCPIP;UID=cvrbind1;PWD=HOT1DOG;";
+    /*
+    "DB": <SBYSA13>,
+    "HOSTNAME": <sbysa13.rochny.ibm.com>,
+    "PORT": <446>, — just in case it is not ssl
+    "USERNAME": <cvrbind1>,
+    "PASSWORD": <HOT1DOG>,
+    "ARM": <fileName.arm>,
+    "SSLPORT": <446></fileName.arm>
+    */
     try {
       var option = { connectTimeout : 40, systemNaming : true };// Connection Timeout after 40 seconds.
       var conn = ibmdb.openSync(connString, option);
-      conn.query("select * from customers fetch first 10 rows only", function (err, rows) {
+      conn.query("SELECT h.ACTIVITYCD FROM HSI.HSIABNOCLO h FETCH FIRST 20 ROWS ONLY;", function (err, rows) {
 		if (err) {
-			console.log(err);
+      console.log(err);
+      res.json(rows);
 		} else {
 		  console.log(rows);
 		}
