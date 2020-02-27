43f7aa3668a644fd0cd5788ff22fc3a0542190a0
José Luis Vargas <joseluisvargas@joses-mbp.gdl.mex.ibm.com>
2020-02-27 15:12:10 -0600

    getting hsi db endpoint

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors=require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIrouter = require('./routes/testAPI');
+var db = require('./routes/db');
+
 
 var app = express();
 
@@ -19,10 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({origin:true,credentials: true}));
+require('dotenv').config()
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI', testAPIrouter);
+app.use('/database', db);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
