a37fd217f7d6f2c3342677cda52264f301031364
Alfredo Guillen <alfredo.guillen1@ibm.com>
2020-02-28 11:30:27 -0600

    Adding some comments and a script

--- controllers/db/db.js
+++ controllers/db/db.js
@@ -5,15 +5,16 @@ var ibmdb = require('ibm_db');
 const dbControllers = {
     getInfoFromDatabase : async function (req, res) {
         let numberOfRows = JSON.parse(req.body.numberOfRows) ? JSON.parse(req.body.numberOfRows) : 20;
-        connString = process.env.CONECTION_DB_STRING;
+        const connString = process.env.CONECTION_DB_STRING;
         try {
-        var option = { connectTimeout : 40, systemNaming : true };// Connection Timeout after 40 seconds.
+        var option = { connectTimeout : 40, systemNaming : true };
         var conn = ibmdb.openSync(connString, option);
         conn.query("SELECT h.QRYID, h.INVNO, h.INVDATE, h.INVAGE FROM HSI.HSIMASTINV h FETCH FIRST " + numberOfRows + " ROWS ONLY;", function (err, rows) {
             if (err) {
                 console.log(err);
                 res.status(400).send({message:err})
             } else {
+                console.log("success" + rows);
                 let count = 0;
                 Object.keys(rows).forEach(function(key) {
                     rows[key].id = "" + count;
